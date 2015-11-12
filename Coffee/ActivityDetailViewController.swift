4c121d2f1920ec8ec276b0f16e1cb49d71d95178
Brian Glowe <brianglowe@gmail.com>
2015-11-12 16:21:17 -0500

    Created custom table view cell for lead list

--- Coffee/ActivityDetailViewController.swift
+++ Coffee/ActivityDetailViewController.swift
@@ -22,6 +22,8 @@ class ActivityDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print("printing the activity date created: \(self.activity?.objectForKey("createdAt"))")
+        
         showActivityDetails()
         // Do any additional setup after loading the view.
     }
